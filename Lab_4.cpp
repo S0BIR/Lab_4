42c4fbe1ce062abfd9afca4dca0bd5574ffa67b4
S0BIR <m.musashayxov@newuu.uz>
2024-09-18 12:39:49 +0500

    Create Lab_4.cpp

---
+++ Lab_4.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+#include "cmath"
+
+//int main() {
+//    int x;
+//    cin>>x;
+//    if (x>=0)
+//        cout<<"It is positive number"<<endl;
+//    else
+//        cout<<"It is negative number"<<endl;
+//}
+
+
+//int main() {
+//    int x;
+//    cin>>x;
+//    if (x%2==0)
+//        cout<<"It is even number"<<endl;
+//    else
+//        cout<<"It is odd number"<<endl;}
+
+
+//int main() {
+//    int a,b,c;
+//    cin>>a>>b>>c;
+//    if ((a>b)&&(a>c))
+//        cout<<a<<endl;
+//    else if ((b>a)&&(b>c))
+//        cout<<b<<endl;
+//    else if((c>a)&&(c>b))
+//        cout<<c<<endl;}
+
+
+//int main(){
+//    int a,b;
+//    cin>>a>>b;
+//    if (a%b==0)
+//        cout<<a<<" is divisible by "<<b<<endl;
+//    else if (b%a==0)
+//        cout<<b<<" is divisible by "<<a<<endl;
+//    else
+//        cout<<a<<" is not divisible by "<<b<<endl;}
+
+
+//int main(){
+//    int year;
+//    cin>>year;
+//    if (year%400==0)
+//        cout<<year <<" is a leap year"<<endl;
+//    else
+//        cout<<year<<" is not a leap year"<<endl;
+//}
+
+
+//int main(){
+//    float a,b,c;
+//    cin>>a>>b>>c;
+//    float d=sqrt(b*b-4*a*c);
+//    cout<<"x1="<<(-1*b+d)/(2*a)<<endl;
+//    cout<<"x2="<<(-1*b-d)/(2*a)<<endl;
+//}
+
+
+//int main(){
+//    int a,b,c;
+//    cin>>a>>b>>c;
+//    if (a+b+c==180)
+//        cout<<"The triangle is valid"<<endl;
+//    else
+//        cout<<"The triangle is not valid"<<endl;
+//}
+
+int main(){
+    char a;
+    cin>>a;
+    
+}
